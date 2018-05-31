19817c80e0461756129b84e65a8402e2631a6708
jugglingcats <alfie@glowbuzzer.com>
2018-05-31 20:50:55 +0100

    General tidy up and work on contact form

--- whr-site/gulpfile.js
+++ whr-site/gulpfile.js
@@ -7,6 +7,10 @@ uglify = require('gulp-uglify'),
     minifyCss = require('gulp-minify-css'),
     rename = require("gulp-rename");
 
+var imgproc = require('gulp-responsive');
+
+var dest = "build/";
+
 // Compile Sass to css and place it in css/styles.css
 gulp.task('styles', function () {
     return gulp.src('src/scss/*.scss')
@@ -15,7 +19,7 @@ gulp.task('styles', function () {
         }))
         .pipe(concat('styles.css'))
         .pipe(autoprefixer('last 2 version', 'ie 9'))
-        .pipe(gulp.dest('css/'))
+        .pipe(gulp.dest(dest + 'css/'))
 });
 
 // Watch for changes in scss files
@@ -24,6 +28,8 @@ gulp.task('watch', function () {
     gulp.watch('src/scss/**/*.scss', ['styles']);
     gulp.watch('src/css/**/*.css', ['styles']);
     gulp.watch('src/js/*.js', ['jshint', 'compress']);
+    gulp.watch('src/img/**', ['images']);
+    gulp.watch('src/*.html', ['html']);
 });
 
 // Catch JS errors
@@ -37,7 +43,70 @@ gulp.task('jshint', function () {
 gulp.task('compress', function () {
     return gulp.src('src/js/*.js')
         .pipe(uglify())
-        .pipe(gulp.dest('js'));
+        .pipe(gulp.dest(dest + 'js'));
+});
+
+gulp.task('video', function () {
+    gulp.src('src/video/whr/**')
+        .pipe(gulp.dest(dest + 'hls'));
+});
+
+gulp.task('html', function () {
+    gulp.src('src/*.html')
+        .pipe(gulp.dest(dest));
+});
+
+gulp.task('ico', function () {
+    gulp.src('src/favicon.ico')
+        .pipe(gulp.dest(dest));
+});
+
+gulp.task('images', function () {
+    return gulp.src('src/img/**')
+        .pipe(imgproc({
+            // Resize all JPG images to three different sizes: 200, 500, and 630 pixels
+            '**/gallery/*.jpg': [{
+                width: 320,
+                rename: {suffix: '-320px'}
+            }, {
+                width: 720,
+                rename: {suffix: '-720px'}
+            }
+                // , {
+                // Compress, strip metadata, and rename original image
+                // rename: {suffix: '-original'}
+                // }
+            ],
+            '*.jpg': [
+                {
+                    width: 720,
+                    rename: {suffix: '-720px'}
+                }, {
+                    width: 1350,
+                    rename: {suffix: '-1350px'}
+                }, {
+                    width: 1920,
+                    rename: {suffix: '-1920px'}
+                }
+                // , {
+                // Compress, strip metadata, and rename original image
+                // rename: {suffix: '-original'}
+                // }
+            ]
+            // Resize all PNG images to be retina ready
+            // '*.png': [{
+            //     width: 250
+            // }, {
+            //     width: 250 * 2,
+            //     rename: {suffix: '@2x'}
+            // }]
+        }, {
+            // global
+            quality: 70,
+            progressive: true,
+            withMetadata: false
+        }))
+        .pipe(gulp.dest(dest + 'img'))
 });
 
 // Minify CSS
@@ -45,8 +114,8 @@ gulp.task('minify-css', function () {
     return gulp.src('css/styles.css')
         .pipe(rename({suffix: ".min"}))
         .pipe(minifyCss({compatibility: 'ie8'}))
-        .pipe(gulp.dest('css'));
+        .pipe(gulp.dest(dest + 'css'));
 });
 
 // All tasks together
-gulp.task('default', ['styles', 'compress', 'jshint', 'watch']);
+gulp.task('default', ['html', 'styles', 'compress', 'jshint', 'images', 'video', 'ico']);
